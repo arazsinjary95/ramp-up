f12079aeaf2aeb1ae5acd6bd147f5d326248c779
Araz Sinjary <araz@Arazs-MacBook-Pro.local>
2018-07-15 22:59:07 +0300

    project creation

--- ramp-up/Controller/RampPlacerVC.swift
+++ ramp-up/Controller/RampPlacerVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  RampPlacerVC.swift
 //  ramp-up
 //
 //  Created by Araz Sinjary on 7/15/18.
@@ -10,7 +10,7 @@ import UIKit
 import SceneKit
 import ARKit
 
-class ViewController: UIViewController, ARSCNViewDelegate {
+class RampPlacerVC: UIViewController, ARSCNViewDelegate {
 
     @IBOutlet var sceneView: ARSCNView!
     
